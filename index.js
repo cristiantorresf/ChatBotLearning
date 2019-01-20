672dc54bba882755468dfeb81c982ef47eaf2e9b
Cristian Torres <cristian.torres19@hotmail.com>
2019-01-20 18:57:15 -0500

    token problems

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 // FaceBook
 
 app.get('/webhook', (req, res) => {
-   if (req.query['hub.verify.token'] === 'blondiebytes') {
+   if (req.query['hub.verify.token'] === 'cristiantorresmitoken') {
       res.send(req.query(['hub.challenge']))
    } else {
       res.send('token invalido');
