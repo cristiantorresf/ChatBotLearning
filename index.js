7d9cd256799c281c70363568f4a77688fca4feea
Cristian Torres <cristian.torres19@hotmail.com>
2019-01-21 19:11:08 -0500

    final working pro

--- index.js
+++ index.js
@@ -1,56 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-const request = require('request');
-require('dotenv').config({
-   path: 'variables.env'
-});
+const verifyWebhook = require('./verify-webhook');
+const messageWebhook = require('./message-webhook');
 
 const app = express();
-app.set('port', process.env.PORT || 5000);
 
+app.set('port', process.env.PORT || 5000)
 
-
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({
-   extended: false
-}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
 
+app.get('/', verifyWebhook);
+app.post('/', messageWebhook);
 
-
-// Rutas
-app.get('/', (req, res) => {
-   res.send('hola bienvenido al servidor');
-   console.log(`mostrando el puerto ${process.env.PORT}`);
-   console.log(`mostrando token de facebook ${process.env.FACEBOOK_ACCESS_TOKEN}`);
-   console.log(`mostrando el token de google ${process.env.DIALOGFLOW_PRIVATE_KEY}`);
-   console.log(`mostrando el email ${process.env.DIALOGFLOW_CLIENT_EMAIL}`);
-})
-
-
-// FaceBook
-
-app.get('/webhook', (req, res) => {
-   let VERIFY_TOKEN = 'cristiantorresmitoken';
-
-   let mode = req.query['hub.mode'];
-   let token = req.query['hub.verify_token'];
-   let challenge = req.query['hub.challenge'];
-
-   if (mode && token === VERIFY_TOKEN) {
-      res.status(200).send(challenge);
-   } else {
-      res.sendStatus(403);
-   }
-});
-
-
-
-
-
-
-app.listen(app.get('port'), () => {
-   console.log(`Servidor a empezado en el puerto ${app.get('port')}`);
-});
+app.listen(app.get('port'), () => console.log('Express server is listening on port ' + app.get('port')));
