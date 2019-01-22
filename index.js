b6724853796c31640684279b597096a808e761f5
Cristian Torres <cristian.torres19@hotmail.com>
2019-01-22 16:32:57 -0500

    blondie bytes way

--- index.js
+++ index.js
@@ -1,97 +1,79 @@
 // Importar las dependencias para configurar el servidor
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
 
-var app = express();
+//process.env.PAGE_ACCESS_TOKEN
+//process.env.VERIFICATION_TOKEN
+
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Allows us to process the data
 app.use(bodyParser.urlencoded({
    extended: false
-}));
-app.use(bodyParser.json());
-// configurar el puerto y el mensaje en caso de exito
-app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escchando!'));
+}))
+app.use(bodyParser.json())
 
-// Ruta de la pagina index
-app.get("/", function (req, res) {
-   res.send("Se ha desplegado de manera exitosa el CMaquera ChatBot :D!!!");
-});
+// ROUTES
 
-// Facebook Webhook
+app.get('/', function (req, res) {
+   res.send("Hi I am a chatbot")
+})
 
-// Usados para la verificacion
-app.get("/webhook", function (req, res) {
-   // Verificar la coincidendia del token
-   if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-      // Mensaje de exito y envio del token requerido
-      console.log("webhook verificado!");
-      res.status(200).send(req.query["hub.challenge"]);
-   } else {
-      // Mensaje de fallo
-      console.error("La verificacion ha fallado, porque los tokens no coinciden");
-      res.sendStatus(403);
+let token = process.env.PAGE_ACCESS_TOKEN;
+
+// Facebook 
+
+app.get('/webhook/', function (req, res) {
+   if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
+      res.send(req.query['hub.challenge'])
    }
-});
+   res.send("Wrong token")
+})
 
-// Todos eventos de mesenger seran capturados por esta ruta
-app.post("/webhook", function (req, res) {
-   // Verificar si el evento proviene del pagina asociada
-   console.log('testing > ' + JSON.stringify(req.body.object));
-   if (req.body.object == "page") {
-      // Si existe multiples entradas entraas
-      req.body.entry.forEach(function (entry) {
-         // Iterara todos lo eventos capturados
-         entry.messaging.forEach(function (event) {
-            if (event.message) {
-               process_event(event);
-            }
-         });
-      });
-      res.sendStatus(200);
-   }
-});
-
-
-// Funcion donde se procesara el evento
-function process_event(event) {
-   // Capturamos los datos del que genera el evento y el mensaje 
-   var senderID = event.sender.id;
-   var message = event.message;
-   console.log(`mensaje recibido ${message}`);
-   // Si en el evento existe un mensaje de tipo texto
-   if (message.text) {
-      // Crear un payload para un simple mensaje de texto
-      var response = {
-         "text": 'Enviaste este mensaje: ' + message.text
+app.post('/webhook/', function (req, res) {
+   let messaging_events = req.body.entry[0].messaging
+   for (let i = 0; i < messaging_events.length; i++) {
+      let event = messaging_events[i]
+      let sender = event.sender.id
+      if (event.message && event.message.text) {
+         let text = event.message.text
+         sendText(sender, "Text echo: " + text.substring(0, 100))
       }
    }
+   res.sendStatus(200)
+})
 
-   // Enviamos el mensaje mediante SendAPI
-   enviar_texto(senderID, response);
-}
-
-// Funcion donde el chat respondera usando SendAPI
-function enviar_texto(senderID, response) {
-   // Construcicon del cuerpo del mensaje
-   let request_body = {
-      "recipient": {
-         "id": senderID
-      },
-      "message": response
+function sendText(sender, text) {
+   let messageData = {
+      text: text
    }
-
-   // Enviar el requisito HTTP a la plataforma de messenger
    request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": {
-         "access_token": process.env.PAGE_ACCESS_TOKEN
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {
+         access_token: token
       },
-      "method": "POST",
-      "json": request_body
-   }, (err, res, body) => {
-      if (!err) {
-         console.log('Mensaje enviado!')
-      } else {
-         console.error("No se puedo enviar el mensaje:" + err);
+      method: "POST",
+      json: {
+         recipient: {
+            id: sender
+         },
+         message: messageData,
       }
-   });
+   }, function (error, response, body) {
+      if (error) {
+         console.log("sending error")
+      } else if (response.body.error) {
+         console.log("response body error")
+      }
+   })
 }
+
+app.listen(app.get('port'), function () {
+   console.log("running: port")
+})
