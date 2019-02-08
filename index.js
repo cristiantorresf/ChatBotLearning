c1b1e4802e6a3b7bec84fe799f5e3a994daa391b
Cristian Torres <cristian.torres19@hotmail.com>
2019-02-08 16:50:18 -0500

    fdialogflow

--- index.js
+++ index.js
@@ -571,56 +571,25 @@ Porque queremos brindarte una asesoria personalizada :)`};
       "attachment": {
         "type": "template",
         "payload": {
-          "template_type": "list",
-          "top_element_style": "compact",
+          "template_type": "generic",
+          
           "elements": [
             {
               "title": "Classic T-Shirt Collection",
               "subtitle": "See all our colors",
-              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+              "image_url": "https://ybxzcgnc7b-flywheel.netdna-ssl.com/wp-content/uploads/2017/08/cool-dog-names-696x435-1-696x435.jpg",          
               "buttons": [
                 {
                   "title": "View",
                   "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                  "url": "https://ybxzcgnc7b-flywheel.netdna-ssl.com/wp-content/uploads/2017/08/cool-dog-names-696x435-1-696x435.jpg",
                   "messenger_extensions": true,
                   "webview_height_ratio": "tall",
-                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                  "fallback_url": "https://ybxzcgnc7b-flywheel.netdna-ssl.com/wp-content/uploads/2017/08/cool-dog-names-696x435-1-696x435.jpg"            
                 }
               ]
             },
-            {
-              "title": "Classic White T-Shirt",
-              "subtitle": "See all our colors",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                "messenger_extensions": false,
-                "webview_height_ratio": "tall"
-              }
-            },
-            {
-              "title": "Classic Blue T-Shirt",
-              "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-              "subtitle": "100% Cotton, 200% Comfortable",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                "messenger_extensions": true,
-                "webview_height_ratio": "tall",
-                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-              },
-              "buttons": [
-                {
-                  "title": "Shop Now",
-                  "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                  "messenger_extensions": true,
-                  "webview_height_ratio": "tall",
-                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                }
-              ]        
-            }
+         
           ],
            "buttons": [
             {
